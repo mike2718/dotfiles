b317ba1aa045ef9fe20dc4661f4e961652e2ed90
Mike Akiba <mike2718@gmail.com>
2021-04-07 19:46:43 +0800

    update

--- bin/amuled.sh
+++ bin/amuled.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-amuled -e -f --use-amuleweb=/usr/local/bin/amuleweb
+#LD_LIBRARY_PATH=/home/mike/usr/wxWidgets/lib:/home/mike/usr/libupnp/lib LANG=en_US.UTF-8 /home/mike/usr/amule/bin/amuled -e -f --use-amuleweb=/home/mike/usr/amule/bin/amuleweb
+LANG=en_US.UTF-8 amuled -e -f --use-amuleweb=/usr/local/bin/amuleweb
