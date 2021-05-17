96f34e653dab5af809405106c7f931bf3216497f
Mike Akiba <mike2718@gmail.com>
2021-05-17 23:43:39 +0800

    update

--- bin/update.sh
+++ bin/update.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-
 sudo apt update
 sudo apt upgrade -y
+
+cd /home/mike/dev/exfat-linux-master && make clean && make && sudo make install
+#sudo modprobe exfat
+#cat /proc/filesystems
+
 
 #sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo update-grub
